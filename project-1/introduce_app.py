9d43372e71124fa3a621dcb9401b538db311136f
blakedebray <blake.debray@gmail.com>
2025-03-11 18:03:46 -0500

    Create Owen's first application!

---
+++ project-1/introduce_app.py
@@ -0,0 +1,21 @@
+#Bite-size Python - project 1: Meet your classmates
+
+#Asking new students these questions to introduce themselves:
+#what is your name
+#what is your favorite sport
+#what is your favorite food
+#what is  your favorite subject
+
+#Introduction
+print('Welcome back to school! Answer these questions to introduce yourself!')
+
+#Store answers to our questions in variables
+name = input('what is your name? ')
+sport = input('what is your favorite sport? ')
+food = input('what is your favorite food? ')
+subject = input('what is  your favorite subject? ')
+
+#Present the answers in a complete sentence
+print()
+print(f"Everyone, meet {name}! {name}'s favorite sport is {sport}. {name}'s favorite food is {food}. {name}'s favorite subject in school is {subject}.")
+print()
